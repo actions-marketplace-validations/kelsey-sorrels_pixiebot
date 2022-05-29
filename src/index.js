79e9e08baa6f1fd2473e35fd5904ffa06c10312f
Kelsey Santos <1139945+kelsey-santos@users.noreply.github.com>
2022-05-28 17:56:42 -0700

    Update formatting

--- src/index.js
+++ src/index.js
@@ -10,6 +10,87 @@ function getSha() {
   }
 }
 
+const names = [
+    "The Fool",
+    "The Magician",
+    "The High Priestess",
+    "The Empress",
+    "The Emperor",
+    "The Hierophant",
+    "The Lovers",
+    "The Chariot",
+    "Strength",
+    "The Hermit",
+    "Wheel of Fortune",
+    "Justice",
+    "The Hanged Man",
+    "Death",
+    "Temperance",
+    "The Devil",
+    "The Tower",
+    "The Star",
+    "The Moon",
+    "The Sun",
+    "Judgement",
+    "The World",
+    "Ace of Wands",
+    "Two of Wands",
+    "Three of Wands",
+    "Four of Wands",
+    "Five of Wands",
+    "Six of Wands",
+    "Seven of Wands",
+    "Eight of Wands",
+    "Nine of Wands",
+    "Ten of Wands",
+    "Page of Wands",
+    "Knight of Wands",
+    "Queen of Wands",
+    "King of Wands",
+    "Ace of Cups",
+    "Two of Cups",
+    "Three of Cups",
+    "Four of Cups",
+    "Five of Cups",
+    "Six of Cups",
+    "Seven of Cups",
+    "Eight of Cups",
+    "Nine of Cups",
+    "Ten of Cups",
+    "Page of Cups",
+    "Knight of Cups",
+    "Queen of Cups",
+    "King of Cups",
+    "Ace of Swords",
+    "Two of Swords",
+    "Three of Swords",
+    "Four of Swords",
+    "Five of Swords",
+    "Six of Swords",
+    "Seven of Swords",
+    "Eight of Swords",
+    "Nine of Swords",
+    "Ten of Swords",
+    "Page of Swords",
+    "Knight of Swords",
+    "Queen of Swords",
+    "King of Swords",
+    "Ace of Pentacles",
+    "Two of Pentacles",
+    "Three of Pentacles",
+    "Four of Pentacles",
+    "Five of Pentacles",
+    "Six of Pentacles",
+    "Seven of Pentacles",
+    "Eight of Pentacles",
+    "Nine of Pentacles",
+    "Ten of Pentacles",
+    "Page of Pentacles",
+    "Knight of Pentacles",
+    "Queen of Pentacles",
+    "King of Pentacles"
+  ];
+
  const urls = [
   "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/RWS_Tarot_00_Fool.jpg/69px-RWS_Tarot_00_Fool.jpg",
   "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/RWS_Tarot_01_Magician.jpg/68px-RWS_Tarot_01_Magician.jpg",
@@ -94,14 +175,20 @@ function draw(commitId) {
     const n = parseInt(commitId, 16) % (78 * 77);
     const n0 = n % 78;
     const n1 = Math.floor(n / 77);
-    return [urls[n0], urls[n1]];
+    return [names[0], urls[n0], names[1], urls[n1]];
 }
 
 function message(commitId) {
-    const [card_url_1, card_url_2] = draw(commitId);
+    const [card_name_1, card_url_1, card_name_2, card_url_2] = draw(commitId);
     return "✨Pixiebot draws✨\n\n" + 
+    '<div style="display:inline-block">' +
     '<img src="' + card_url_1 + '" />' +
-    '<img src="' + card_url_2 + '" />';
+    '<p>' + card_name_1 + '</p>' +
+    '</div>' +
+    '<div style="display:inline-block">' +
+    '<img src="' + card_url_2 + '" />' +
+    '<p>' + card_name_2 + '</p>' +
+    '</div>';
 }
 
 async function run() {
